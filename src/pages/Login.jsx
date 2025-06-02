aa51e25de05738bffaae18957eaa1a687124b734
Raghava3124 <irvsatyanarayana@gmail.com>
2025-06-02 13:40:13 +0530

    added new url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,8 @@ const Login = ({ onLogin }) => {
         setMessage("");
 
         try {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            // const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch("https://ecom-production-ca19.up.railway.app/api/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
