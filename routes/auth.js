9708100dbc6c8e4d48543cca6c5202b198b473ca
Raghava3124 <irvsatyanarayana@gmail.com>
2025-09-13 10:04:56 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -57,11 +57,22 @@ const SECRET_KEY = "your_secret_key"; // Use environment variables instead
 //   queueLimit: 0
 // });
 
+// const db = mysql.createPool({
+//   host: 'mainline.proxy.rlwy.net',
+//   port: 48545,
+//   user: 'root',
+//   password: 'sYoqlnefIoidfztNAwQwCrYlqFCOXwnV',
+//   database: 'railway',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
 const db = mysql.createPool({
-  host: 'mainline.proxy.rlwy.net',
-  port: 48545,
+  host: 'switchyard.proxy.rlwy.net',
+  port: 29003,
   user: 'root',
-  password: 'sYoqlnefIoidfztNAwQwCrYlqFCOXwnV',
+  password: 'EeOPsqPycwUkiAJCYYhxcjhVkkmDogpa',
   database: 'railway',
   waitForConnections: true,
   connectionLimit: 10,
